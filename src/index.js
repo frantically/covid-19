7ea0a20cf14bc40866c4c826fa6ed16ee69ccd56
Tim Parker <parker_tim@yahoo.co.uk>
2020-11-18 22:36:01 +0100

    Refactoring and carry fwd logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,108 @@
+// TODO
+// UNIT TESTS
+// VALIDATE DATA AGAINST CORONA-DATA.CH THIS IS LOWER
+// KEYS SAMPLE_DATA_ is there a better way to do this?
+// SPARK LINES FOR THE LAST 7 DAYS?
+// LATEST DATE ISN'T LOOKING AT COMPLETE DAYS ONLY
+
 const PROMINENT_SERIES_ALPHA = 1
 const DEFAULT_SERIES_ALPHA = 0.5
 
 const SAMPLE_DATA_FIELDS_SOURCE = ["ncumul_tested" , "ncumul_conf" , "new_hosp" , "current_hosp", "current_icu" , "current_vent" , "ncumul_released" , "ncumul_deceased", "current_isolated", "current_quarantined" , "current_quarantined_riskareatravel", "current_quarantined_total" ]
 const SAMPLE_DATA_FIELDS_ALL = SAMPLE_DATA_FIELDS_SOURCE.concat(["conf", "deceased"])
+const SAMPLE_DATA_FIELDS_CARRY = ["ncumul_conf", "ncumul_deceased"]
+const SAMPLE_DATA_FIELDS_ALL_NEW = SAMPLE_DATA_FIELDS_SOURCE.concat(["ncumul_conf_raw", "ncumul_deceased_raw"])
+
+const ONE_DAY = 24 * 60 * 60 * 1000
 
 var cantonConfig = {}
+
+function scaffoldData(source) {
+    var minMax = source.reduce((a, c) => {
+        a.min = Math.min(c.date, a.min)
+        a.max = Math.max(c.date, a.max)
+        return a
+    }, {min: Number.MAX_VALUE , max: 0})
+    var days = []
+    for(i = minMax.min;i<= minMax.max;i = i+ONE_DAY) {
+        days.push(i)
+    }
+    var cantons = getCantons(source)
+    var result = {}
+    cantons.forEach(canton => {
+        var cantonData = getCantonDataByDate(source, canton)
+        var cantonScaffoldedData = days.map(date => {
+            return cantonData[date] ? cantonData[date] : {date: date, abbreviation_canton_and_fl: canton}
+        })
+        result[canton] = cantonScaffoldedData
+    })
+    carryForwardAccumlatedValues(result)
+    result['CH'] = aggregateCH(result)
+    return result
+}
+
+function carryForwardAccumlatedValues(data) {
+    Object.values(data).forEach(cantonData => {
+        var lastValue = { }
+
+        //carry forward current value if new value not provided
+        cantonData.forEach(sample => {
+            SAMPLE_DATA_FIELDS_CARRY.forEach(field => {
+                sample[`${field}_raw`] = sample[field]
+                if(isNaN(sample[field])) {
+                    sample[field] = lastValue[field] || 0
+                } else {
+                    lastValue[field] = sample[field]
+                }
+            })
+        })
+        
+        //but wipe out any extrapolation after the last value in the underlying data source
+        SAMPLE_DATA_FIELDS_CARRY.forEach(field => {
+            var i = cantonData.length - 1
+            while(isNaN(cantonData[i][`${field}_raw`])) {
+                cantonData[i][field] = NaN
+                i--
+            }
+        })
+    })
+}
+
+function aggregateCH(data) {
+    var cantonsData = Object.values(data)
+    var sampleCount = cantonsData[0].length
+    var result = []
+    for(i=0;i<sampleCount;i++) {
+        var sample = {date: cantonsData[0][i].date, abbreviation_canton_and_fl: "CH" }
+        SAMPLE_DATA_FIELDS_ALL_NEW.forEach(field => {
+            var value = cantonsData
+                .map(cantonValues => cantonValues[i][field])
+                .reduce((a, v) => a + v, 0)
+            sample[field] = value
+        })
+        result.push(sample)
+    }
+    return result
+}
+
+function getCantonDataByDate(data, canton) {
+    var result = {}
+    data
+        .filter(sample => sample.abbreviation_canton_and_fl == canton)
+        .forEach(sample => {
+            result[sample.date] = sample
+        })
+    return result
+}
+
+function getCantons(data) {
+    var cantons = data.reduce((a, c) => {
+        a.add(c.abbreviation_canton_and_fl)
+        return a
+    }, new Set())
+    
+    return [...cantons]
+}
 
 function formatNumber(n) {
     return new Number(n).toLocaleString('de-CH')
@@ -12,79 +110,6 @@ function formatNumber(n) {
 
 function formatDate(d) {
     return `${new Date(d).toLocaleDateString("de-CH")}`
-}
-
-function projectIncompleteData(data) {
-    var maxDate = data.reduce((a, c) => Math.max(a, c.date), Date.parse("2020-01-01"))
-    var cantons = data.reduce((a, c) => {
-        a.add(c.abbreviation_canton_and_fl)
-        return a
-    }, new Set())
-    
-    cantons = [...cantons]
-
-    var missingEndDateSamples = cantons
-        .map(canton => {
-            var cantonData = data.filter(sample => sample.abbreviation_canton_and_fl == canton)
-            return cantonData[cantonData.length - 1]
-        })
-        .filter(cantonMax => cantonMax.date < maxDate)
-        .map(cantonMax => {
-            return {abbreviation_canton_and_fl: cantonMax.abbreviation_canton_and_fl, date: maxDate}
-        })
-    data = data.concat(missingEndDateSamples)
-
-    var mostRecentSampleValuesByCanton = {}
-    cantons
-        .map(canton => data.filter(sample => sample.abbreviation_canton_and_fl == canton))
-        .map(cantonData => {
-            var result = {}
-            cantonData.forEach(sample => {
-                SAMPLE_DATA_FIELDS_ALL.forEach(field => {
-                    result[field] = isNaN(sample[field]) ? result[field] : sample[field]
-                })
-            })
-            result.abbreviation_canton_and_fl = cantonData[0].abbreviation_canton_and_fl
-            result.date = maxDate
-            return result
-        })
-        .forEach(carryForward => {mostRecentSampleValuesByCanton[carryForward.abbreviation_canton_and_fl] = carryForward})
-    
-    //now apply the carry forwards to the last sample for a canton
-    cantons
-        .map(canton => {
-            var cantonData = data.filter(sample => sample.abbreviation_canton_and_fl == canton)
-            return cantonData[cantonData.length - 1]
-        })
-        .forEach(cantonMax =>{
-            SAMPLE_DATA_FIELDS_ALL.forEach(field => {
-                cantonMax[field] = isNaN(cantonMax[field]) ? mostRecentSampleValuesByCanton[cantonMax.abbreviation_canton_and_fl][field] : cantonMax[field]
-            })
-        })
-    return data
-}
-
-function createCHData(data) {
-    var result = data.reduce((result, sample) => {
-        var dateResult = result[sample.date]
-        if(!dateResult) {
-            dateResult = {
-                date: sample.date,
-                abbreviation_canton_and_fl: "CH",
-                samples: 0
-            }
-            result[sample.date] = dateResult
-        }
-        result[sample.date] = dateResult //TODO: can this be done only if null in the line above?
-
-        SAMPLE_DATA_FIELDS_ALL.forEach(field => {
-            dateResult[field] = (dateResult[field]?dateResult[field]+sample[field]:sample[field])
-        })
-        dateResult.samples++
-        return result
-    }, {})
-    //return Object.values(result) //need a way to project forward on most recent data - look for max and add in?
-    return Object.values(result).filter(sample => sample.samples > 23)
 }
 
 function convertCSVToJSON(str, delimiter = ',') {
@@ -110,7 +135,6 @@ function chartSeries(data, label, rgb, alpha) {
     }
     return result
 }
-
 
 function chartOptions() {
     return {
@@ -151,68 +175,52 @@ function chartOptions() {
     }
 }
 
-function getMovingAverage(series, days) {
-    var movingAverage = []
-    return series
-        .map(point => {
-            movingAverage.push(point.y)
-            while(movingAverage.length > days) {
-                movingAverage.shift()
-            }
-            if(movingAverage.length == days) {
-                return {x: point.x, y: movingAverage.reduce((a, c) => a+c, 0)/days}
-            } else {
-                return {x: point.x, y: NaN}
-            }
-        })
+function getMovingAverage(data, accumulativeField, days) {
+    var result = []
+    for(i = days -1 ;i<data.length;i++) {
+        var point = {x: data[i].date, y: Math.round((data[i][accumulativeField]-data[i-days+1][accumulativeField])/days)}
+        result.push(point)
+    }
+    return result
 }
 
-//TODO: Move accumulation -> conf into a generic function?
 function parseData(data) {
     var data = convertCSVToJSON(data)
-    var previous_ncumul_conf = {}
-    var previous_ncumul_deceased = {}
     data.forEach(item => {
         item.date = Date.parse(item.date)
         SAMPLE_DATA_FIELDS_SOURCE.forEach(field => {
             item[field] = parseInt(item[field])
         })
-        var previousConf = previous_ncumul_conf[item.abbreviation_canton_and_fl] || 0
-        item.conf = item.ncumul_conf - previousConf
-        previous_ncumul_conf[item.abbreviation_canton_and_fl] = item.ncumul_conf
-
-        var previousDeceased = previous_ncumul_deceased[item.abbreviation_canton_and_fl] || 0
-        item.deceased = item.ncumul_deceased - previousDeceased
-        previous_ncumul_deceased[item.abbreviation_canton_and_fl] = item.ncumul_deceased
     })
     return data.filter(sample => sample.date) // parsing the CSV gets an empty row at the end
 }
 
-function createSeriesData(data, key, canton) {
+function createSeriesData(data, key) {
     return data
-        .filter(sample => sample.abbreviation_canton_and_fl == canton)
-        .map(sample => {return {x: sample.date, y: sample[key] < 0 ? 0 : sample[key]}})
+        .map(sample => {return {x: sample.date, y: sample[key]}})
 }
 
 function addNumericalStats(data) {
-    var conf = createSeriesData(data, 'conf', 'CH')
-    var ncumul_conf = createSeriesData(data, 'ncumul_conf', 'CH')
 
-    var deceased = createSeriesData(data, 'deceased', 'CH')
-    var ncumul_deceased = createSeriesData(data, 'ncumul_deceased', 'CH')
+    var lastWeekCumulative = data['CH'].map(d => d.ncumul_conf).filter(s => s).slice(-7)
+    var priorWeekCumulative = data['CH'].map(d => d.ncumul_conf).filter(s => s).slice(-14)
+    var lastWeek = lastWeekCumulative[6]-lastWeekCumulative[0]
+    var priorWeek = priorWeekCumulative[6]-priorWeekCumulative[0]
 
-    var lastWeek = conf.slice(-7).map(e => e.y).reduce((a, c) => a+c, 0)
-    var priorWeek = conf.slice(-14).map(e => e.y).reduce((a, c) => a+c, 0) - lastWeek
+    var lastWeekDeceasedCumulative = data['CH'].map(d => d.ncumul_deceased).filter(s => s).slice(-7)
+    var priorWeekDeceasedCumulative = data['CH'].map(d => d.ncumul_deceased).filter(s => s).slice(-14)
+    var lastWeekDeceased = lastWeekDeceasedCumulative[6]-lastWeekDeceasedCumulative[0]
+    var priorWeekDeceased = priorWeekDeceasedCumulative[6]-priorWeekDeceasedCumulative[0]
 
-    var lastWeekDeaths = deceased.slice(-7).map(e => e.y).reduce((a, c) => a+c, 0)
-    var priorWeekDeaths = deceased.slice(-14).map(e => e.y).reduce((a, c) => a+c, 0) - lastWeekDeaths
+    var ncumul_conf = data['CH'].map(d => d.ncumul_conf).filter(s => s).slice(-1)[0]
+    var ncumul_deceased = data['CH'].map(d => d.ncumul_deceased).filter(s => s).slice(-1)[0]
 
-    var maxDate = data.reduce((result, current) => Math.max(result, current.date), 0)
+    var maxDate = data['CH'].reduce((result, current) => Math.max(result, current.date), 0)
 
-    document.getElementById("totalConfirmed").innerHTML = formatNumber(ncumul_conf[ncumul_conf.length-1].y);
+    document.getElementById("totalConfirmed").innerHTML = formatNumber(ncumul_conf);
     document.getElementById("last7Days").innerHTML = `<span class="${lastWeek > priorWeek ? "down" : "up"}">${formatNumber(lastWeek)}</span>`
-    document.getElementById("totalDeaths").innerHTML = formatNumber(ncumul_deceased[ncumul_deceased.length-1].y)
-    document.getElementById("last7Deaths").innerHTML = `<span class="${lastWeekDeaths > priorWeekDeaths ? "down" : "up"}">${formatNumber(lastWeekDeaths)}</span>`
+    document.getElementById("totalDeaths").innerHTML = formatNumber(ncumul_deceased)
+    document.getElementById("last7Deaths").innerHTML = `<span class="${lastWeekDeceased > priorWeekDeceased ? "down" : "up"}">${formatNumber(lastWeekDeceased)}</span>`
     document.getElementById("maxDate").innerHTML = `${formatDate(maxDate)}`
     }
 
@@ -223,8 +231,8 @@ function addCases(data) {
         type: 'line',
         data: {
             datasets: [ 
-                chartSeries(createSeriesData(data, 'ncumul_conf', 'ZH'), "ZH", cantonConfig.ZH.color, PROMINENT_SERIES_ALPHA),
-                chartSeries(createSeriesData(data, 'ncumul_conf', 'CH'), "CH", cantonConfig.CH.color, PROMINENT_SERIES_ALPHA),
+                chartSeries(createSeriesData(data['ZH'], 'ncumul_conf'), "ZH", cantonConfig.ZH.color, PROMINENT_SERIES_ALPHA),
+                chartSeries(createSeriesData(data['CH'], 'ncumul_conf'), "CH", cantonConfig.CH.color, PROMINENT_SERIES_ALPHA),
             ]
         },
         options: chartOptions()
@@ -232,14 +240,15 @@ function addCases(data) {
 }
 
 function addDeaths(data) {
+
     var ctx = document.getElementById('deaths');
 
     new Chart(ctx, {
         type: 'line',
         data: {
             datasets: [ 
-                chartSeries(getMovingAverage(createSeriesData(data, 'deceased', 'ZH'), 7), "ZH", cantonConfig.ZH.color, PROMINENT_SERIES_ALPHA),
-                chartSeries(getMovingAverage(createSeriesData(data, 'deceased', 'CH'), 7), "CH", cantonConfig.CH.color, PROMINENT_SERIES_ALPHA),
+                chartSeries(getMovingAverage(data['ZH'], 'ncumul_deceased', 7), 'ZH', cantonConfig.ZH.color, PROMINENT_SERIES_ALPHA),
+                chartSeries(getMovingAverage(data['CH'], 'ncumul_deceased', 7), 'CH', cantonConfig.CH.color, PROMINENT_SERIES_ALPHA),
             ]
         },
         options: chartOptions()
@@ -251,6 +260,10 @@ function getCasesPer100k(data, canton) {
     return cases.map(point => { return {x: point.x, y: point.y/(cantonConfig[canton].population/100000)}})
 }
 
+function per100k(series, canton) {
+    return series.map(point => { return {x: point.x, y: point.y/(cantonConfig[canton].population/100000)}})
+}
+
 function addCasesPer100000(data) {
     var ctx = document.getElementById('casesPer100000');
 
@@ -258,10 +271,10 @@ function addCasesPer100000(data) {
         type: 'line',
         data: {
             datasets: [ 
-                chartSeries(getMovingAverage(getCasesPer100k(data, "ZH"), 7), "ZH", cantonConfig.ZH.color, PROMINENT_SERIES_ALPHA),
-                chartSeries(getMovingAverage(getCasesPer100k(data, "ZG"), 7), "ZG", cantonConfig.ZG.color, PROMINENT_SERIES_ALPHA),
-                chartSeries(getMovingAverage(getCasesPer100k(data, "GR"), 7), "GR", cantonConfig.GR.color, PROMINENT_SERIES_ALPHA),
-                chartSeries(getMovingAverage(getCasesPer100k(data, "CH"), 7), "CH", cantonConfig.CH.color, PROMINENT_SERIES_ALPHA),
+                chartSeries(per100k(getMovingAverage(data['ZH'], 'ncumul_conf', 7), 'ZH'), 'ZH', cantonConfig.ZH.color, PROMINENT_SERIES_ALPHA),
+                chartSeries(per100k(getMovingAverage(data['ZG'], 'ncumul_conf', 7), 'ZG'), 'ZG', cantonConfig.ZG.color, PROMINENT_SERIES_ALPHA),
+                chartSeries(per100k(getMovingAverage(data['GR'], 'ncumul_conf', 7), 'GR'), 'GR', cantonConfig.GR.color, PROMINENT_SERIES_ALPHA),
+                chartSeries(per100k(getMovingAverage(data['CH'], 'ncumul_conf', 7), 'CH'), 'CH', cantonConfig.CH.color, PROMINENT_SERIES_ALPHA),
             ]
         },
         options: chartOptions()
@@ -275,13 +288,16 @@ function addHospital(data) {
         type: 'line',
         data: {
             datasets: [
-                chartSeries(createSeriesData(data, 'current_hosp', 'ZH'), 'Hospitalized', [252, 191, 73], PROMINENT_SERIES_ALPHA), 
-                chartSeries(createSeriesData(data, 'current_icu', 'ZH'), 'ICU', [247, 127, 0], PROMINENT_SERIES_ALPHA),
-                chartSeries(createSeriesData(data, 'current_vent', 'ZH'), 'Ventilated', [214, 40, 40], PROMINENT_SERIES_ALPHA),
+                chartSeries(createSeriesData(data['ZH'], 'current_hosp'), 'Hospitalized', [252, 191, 73], PROMINENT_SERIES_ALPHA),
+                chartSeries(createSeriesData(data['ZH'], 'current_icu'), 'ICU', [247, 127, 0], PROMINENT_SERIES_ALPHA),
+                chartSeries(createSeriesData(data['ZH'], 'current_vent'), 'Ventilated', [214, 40, 40], PROMINENT_SERIES_ALPHA),
             ]
         },
         options: chartOptions()
     });
+}
+
+function outputDebug(newData) {
 }
 
 //TODO: could do two fetches in parallel
@@ -302,15 +318,15 @@ function init() {
         // .then(x => { return fetch('data.csv')})
         .then(x => { return fetch('https://raw.githubusercontent.com/openZH/covid_19/master/COVID19_Fallzahlen_CH_total_v2.csv')})
         .then(r => r.text())
-        .then(data => {
-            data = parseData(data)
-            data = data.concat(createCHData(data))
-            data = projectIncompleteData(data)
-            addNumericalStats(data)
-            addCases(data)
-            addCasesPer100000(data)
-            addHospital(data)
-            addDeaths(data)
+        .then(csvData => {
+            newData = parseData(csvData)
+            newData = scaffoldData(newData)
+            addNumericalStats(newData)
+            addCasesPer100000(newData)
+            addCases(newData)
+            addHospital(newData)
+            addDeaths(newData)
+            outputDebug(newData)
         })
 }
 
